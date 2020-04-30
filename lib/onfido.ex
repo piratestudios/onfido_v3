9dbbdb3e970426ecf96a9366a78b52a63e543690
Becky Smith <beckysmith@piratestudios.co.uk>
2020-04-30 17:17:05 +0100

    Added module doc

--- lib/onfido.ex
+++ lib/onfido.ex
@@ -1,2 +1,9 @@
 defmodule Onfido do
+  @moduledoc """
+  Elixir wrapper of the [Onfido](https://onfido.com) API.
+
+  [View it in Git](https://github.com/becky-pirate/onfido).
+
+  Forked from [https://github.com/PiggyPot/onfido](https://github.com/PiggyPot/onfido) and updated for v3 of Onfido API.
+  """
 end
