a34e69783da3c9ff61db2ebdf810bc8b06bbe60e
Becky Smith <beckysmith@piratestudios.co.uk>
2020-05-19 17:17:27 +0100

    Added method verify webhook signature

--- lib/resources/webhook.ex
+++ lib/resources/webhook.ex
@@ -41,4 +41,33 @@ defmodule Onfido.Resources.Webhook do
   def register(params) do
     HttpDriver.request(:post, "/webhooks", params)
   end
+
+  @doc """
+  Verify signature on requests to Onfido webhook
+  Based on https://developers.onfido.com/guide/manual-webhook-signature-verification
+
+  Signature comparison requires raw_event_body as a string
+  Once signature is verified it is safe to parse raw_event_body returned from this function
+
+  # If using Plug, get raw_event_body and signature_header like so:
+
+        {:ok, raw_event_body, conn} = read_body(conn)
+        signature_header =
+          get_req_header(conn, "X-SHA2-Signature")
+          |> Base.decode64()
+    
+  ## Example
+
+        Onfido.Resources.Webhook.verify_signature(webhook_token, signature_header, raw_event_body)
+  """
+  def verify_webhook_signature(webhook_token, signature_header, raw_event_body) do
+    # Compute the HMAC using the SHA256 algorithm and using your webhook's token as the key.
+    expected_signature = :crypto.hmac(:sha256, webhook_token, raw_event_body)
+
+    # Must be a constant time comparison to prevent timing attacks - Elixir's pattern matching operations are constant time
+    case {signature_header, expected_signature} do
+      {equal, equal} -> {:ok, raw_event_body}
+      {_, _} -> {:error, "invalid request signature on Onfido webhook"}
+    end
+  end
 end
